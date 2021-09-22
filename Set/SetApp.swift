fdb3750583d45e4e2ae4c46f653cbaadb76bb359
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-09-22 16:20:54 +0200

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Gabriel Haugbøl on 22/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
